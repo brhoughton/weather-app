7696525f13a6a3d131a1fd6fce253cab43e09577
Breck Houghton <breck.houghton@gmail.com>
2024-03-11 15:26:16 -0700

    Adjusted background styling

--- iconMap.js
+++ iconMap.js
@@ -5,14 +5,14 @@ addMapping([2], "cloud-sun");
 addMapping([3], "cloud");
 addMapping([45, 48], "smog");
 addMapping(
-  [51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82],
-  "cloud-showers-heavy "
+	[51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82],
+	"cloud-showers-heavy"
 );
 addMapping([71, 73, 75, 77, 85, 86], "snowflake");
-addMapping([96, 96, 99], "cloud-bolt");
+addMapping([95, 96, 99], "cloud-bolt");
 
 function addMapping(values, icon) {
-  values.forEach((value) => {
-    ICON_MAP.set(value, icon);
-  });
+	values.forEach((value) => {
+		ICON_MAP.set(value, icon);
+	});
 }
